23747de71d56907eac4b28b6aa534cd30e47afad
Stephen Rhoton <stephen.rhoton@dealerware.com>
2024-03-06 13:22:05 -0500

    Firehose config added

--- kinesis.tf
+++ kinesis.tf
@@ -1,7 +1,7 @@
 resource "aws_kinesis_stream" "affiliate_stream" {
-  name             = var.stream_name
-  shard_count      = var.shard_count
+  name                = var.stream_name
+  shard_count         = var.shard_count
   shard_level_metrics = var.shard_level_metrics
-  retention_period = 24
-  tags = { for tag_key, tag_value in local.merged_tags : tag_key => tag_value}
+  retention_period    = 24
+  tags                = { for tag_key, tag_value in local.merged_tags : tag_key => tag_value }
 }
