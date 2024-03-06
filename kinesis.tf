0efb8f6586c13a6efd8f1657cf61e92459bcf7fe
Stephen Rhoton <stephen.rhoton@dealerware.com>
2024-03-06 11:32:51 -0500

    Fix tagging config

--- kinesis.tf
+++ kinesis.tf
@@ -3,7 +3,5 @@ resource "aws_kinesis_stream" "affiliate_stream" {
   shard_count      = var.shard_count
   shard_level_metrics = var.shard_level_metrics
   retention_period = 24
-  tags = {
-    Environment = "Dev"
-  }
+  tags = { for tag_key, tag_value in local.merged_tags : tag_key => tag_value}
 }
