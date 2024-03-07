64589c7bc4f83720c2e48c4c3726f0c3886dd46e
Stephen Rhoton <stephen.rhoton@dealerware.com>
2024-03-07 13:48:06 -0500

    Fix description

--- variables.tf
+++ variables.tf
@@ -42,7 +42,7 @@ variable "shard_level_metrics" {
 }
 
 variable "target_bucket_name_prefix" {
-  description = "The name of the S3 bucket to use as the target for the Kinesis Firehose delivery stream"
+  description = "The prefix of the S3 bucket to use as the target for the Kinesis Firehose delivery stream"
   type        = string
   default     = "firehose-target-bucket"
 }
