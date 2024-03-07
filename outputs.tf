9e3e4006a103b633a6db9ce7bbb8bd555aa63d71
Stephen Rhoton <stephen.rhoton@dealerware.com>
2024-03-07 12:36:05 -0500

    Add the bucket name to the outputs.tf file

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,11 @@ output "s3_bucket_id" {
   description = "The ID of the S3 bucket"
 }
 
+output "s3_bucket_name" {
+  value       = aws_s3_bucket.firehose_target.bucket
+  description = "The name of the S3 bucket"
+  
+}
 output "s3_bucket_arn" {
   value       = aws_s3_bucket.firehose_target.arn
   description = "The ARN of the S3 bucket"
